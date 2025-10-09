f756281f1f748d7e174fae8e1371933491df39da
cosmiix <dm_d00@mail.ru>
2025-10-09 20:55:03 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCB_bwtspz0GuQeialypinwih1VQ1E30wo",
+  authDomain: "my-schedule-b323f.firebaseapp.com",
+  projectId: "my-schedule-b323f",
+  storageBucket: "my-schedule-b323f.firebasestorage.app",
+  messagingSenderId: "2949606491",
+  appId: "1:2949606491:web:2a0f37da954c0d1267237e"
+};
+
+const ADMIN_PASSWORD_HASH = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92";
+const MANAGER_PASSWORD_HASH = "b2d78a0f6f3d76b28d5367d65bdd031f6704f25f2d3fe6c3d2d3b4a6f6c1a2c6";
